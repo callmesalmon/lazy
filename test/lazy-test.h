e3c02bab0c6b7425a88cad3fb780c3a202f940f1
eliss <elis.staaf@proton.me>
2025-01-10 10:35:36 +0100

    fix tests

--- test/lazy-test.h
+++ test/lazy-test.h
@@ -6,3 +6,19 @@ void test_math_funcs() {
     assert(mod(4, 5) != 0);
     assert(knuth(2, 2, 4) != 0);
 }
+
+void test_python_macros() {
+#ifndef __cplusplus
+    assert(not 1 == 2);
+    assert(1 == 2 or 2 == 2);
+    assert(1 == 1 and 2 == 2);
+#endif
+}
+
+#define test_lua_macros function() do \
+    return 256;                       \
+end;                                  \
+
+fn test_function_macros() do
+    return 0;
+end
--- test/main.c
+++ test/main.c
@@ -11,6 +11,9 @@ int main() {
 	test_try_catch_macros();
 	test_print_macros();
     test_math_funcs();
+    test_python_macros();
+    test_function_macros();
+    test_lua_macros;
 
     return 0;
 }
